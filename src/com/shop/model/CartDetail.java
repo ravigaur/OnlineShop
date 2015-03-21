8e338753c37055edbbb0240017206a8b63938766
ravigaur <ravigaur.mca@rediffmail.com>
2015-03-21 20:11:44 +0530

    updated comment

--- src/com/shop/model/CartDetail.java
+++ src/com/shop/model/CartDetail.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class CartDetail
+ * Servlet implementation class CartDetail with demo
  */
 
 public class CartDetail implements  ServletContextListener {
